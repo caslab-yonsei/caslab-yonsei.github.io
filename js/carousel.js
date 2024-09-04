7477150390419d5a3e8dc1a796946f88aadb70d8
Seungkyu Lee <lee.seungkyu@dgist.ac.kr>
2024-09-04 19:46:53 +0900

    carousel 7

--- js/carousel.js
+++ js/carousel.js
@@ -7,6 +7,15 @@ let startX, startScrollLeft;
 
 carouselBtn.forEach(btn => {
     btn.addEventListener("click", () => {
+        if (btn.id === "left") {
+            if (carousel.scrollLeft === 0) {
+                carousel.scrollLeft = carousel.scrollWidth - (3 * carousel.offsetwidth);
+            } else carousel.scrollLeft -= firstItemWidth;
+        } else {
+            if (carousel.scrollLeft === carousel.scrollWidth - carousel.offsetWidth) {
+                carousel.scrollLeft = 0;
+            } else carousel.scrollLeft += firstItemWidth;
+        }
         carousel.scrollLeft += btn.id === "left" ? -firstItemWidth : firstItemWidth;
     });
 });
@@ -14,13 +23,13 @@ carouselBtn.forEach(btn => {
 const dragStart = (e) => {
     isDragging = true;
     carousel.classList.add("dragging");
-    startX = e.pageX;
+    startX = e.pageX || e.touches[0].pageX;
     startScrollLeft = carousel.scrollLeft;
 }
 
 const dragging = (e) => {
     if (!isDragging) return;
-    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+    carousel.scrollLeft = startScrollLeft - ((e.pageX || e.touches[0].pageX) - startX);
 }
 
 const dragStop = () => {
@@ -28,6 +37,22 @@ const dragStop = () => {
     carousel.classList.remove("dragging");
 }
 
+const infiniteScroll = () => {
+    if (carousel.scrollLeft === 0) {
+        carousel.scrollLeft = carousel.scrollWidth - (3 * carousel.offsetWidth);
+    } else if (carousel.scrollLeft === carousel.scrollWidth - carousel.offsetWidth) {
+        carousel.scrollLeft = 0;
+    }
+}
+
 carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+
 carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+
 carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("mouseleave", dragStop);
+carousel.addEventListener("touchleave", dragStop);
+
+carousel.addEventListener("scroll", infiniteScroll);
