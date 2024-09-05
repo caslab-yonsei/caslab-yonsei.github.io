d2ecf92dd0ace97bd92aa4c219fd175b93e0352a
Seungkyu Lee <lee.seungkyu@dgist.ac.kr>
2024-09-05 17:23:30 +0900

    carousel 17 (touch, auto fix)

--- js/carousel.js
+++ js/carousel.js
@@ -3,7 +3,7 @@ carouselFirst = carousel.querySelectorAll(".carousel-item")[0];
 carouselControls = document.querySelectorAll(".carousel-control");
 
 let isDragStart = false;
-let prevPageX, prevScrollLeft;
+let prevPageX, prevScrollLeft, positionDiff;
 let firstItemWidth = carouselFirst.clientWidth + 12;
 let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
 
@@ -19,9 +19,21 @@ carouselControls.forEach(btn => {
     })
 });
 
+const autoSlide = () => {
+    positionDiff = Math.abs(positionDiff);
+    let firstItemWidth = carouselFirst.clientWidth + 12;
+    let valDiff = firstItemWidth - positionDiff;
+
+    if (carousel.scrollLeft > prevScrollLeft) {
+        carousel.scrollLeft += positionDiff > firstItemWidth / 3 ? valDiff : -positionDiff;
+    } else {
+        carousel.scrollLeft -= positionDiff > firstItemWidth / 3 ? valDiff : -positionDiff;
+    }
+}
+
 const dragStart = (e) => {
     isDragStart = true;
-    prevPageX = e.pageX;
+    prevPageX = e.pageX || e.touches[0].pageX;
     prevScrollLeft = carousel.scrollLeft;
 }
 
@@ -29,15 +41,22 @@ const dragging = (e) => {
     if (!isDragStart) return;
     e.preventDefault();
     carousel.classList.add("dragging");
-    let positionDiff = e.pageX - prevPageX;
+    let positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
     carousel.scrollLeft = prevScrollLeft - positionDiff;
 }
 
 const dragStop = () => {
     isDragStart = false;
     carousel.classList.remove("dragging");
+    autoSlide();
 }
 
 carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+
 carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+
 carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("mouseleave", dragStop);
+carousel.addEventListener("touchend", dragStop);
