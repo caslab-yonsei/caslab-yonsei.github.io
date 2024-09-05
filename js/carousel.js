39849c6af6a1dac46ff8c351b6a4119c92908b10
Seungkyu Lee <lee.seungkyu@dgist.ac.kr>
2024-09-05 16:42:32 +0900

    js for carousel control

---
+++ js/carousel.js
@@ -0,0 +1,43 @@
+const carousel = document.querySelector(".carousel");
+const carouselFirst = carousel.querySelectorAll(".carousel-item")[0];
+const carouselControls = documnet.querySelectorAll(".carousel-control");
+
+let isDragStart = false;
+let prevPageX, prevScrollLeft;
+let firstItemWidth = carouselFirst.clientWidth + 12;
+let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
+
+carouselControls.forEach(btn => {
+    btn.addEventListener("click", () => {
+        if (carousel.scrollLeft <= 0 && btn.id === "cc-left") {
+            carousel.scrollLeft = scrollWidth;
+        } else if (carousel.scrollLeft >= scrollWidth && btn.id === "cc-right") {
+            carousel.scrollLeft = 0;
+        } else {
+            carousel.scrollLeft += btn.id === "cc-left" ? -firstItemWidth : firstItemWidth;
+        }
+    })
+});
+
+const dragStart = (e) => {
+    isDragStart = true;
+    prevPageX = e.pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    if (!isDragStart) return;
+    e.preventDefault();
+    carousel.classList.add("dragging");
+    let positionDiff = e.pageX - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+}
+
+const dragStop = () => {
+    isDragStart = false;
+    carousel.classList.remove("dragging");
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
